470d7f50dd4544f5195f4b327d513bc1b417a48d
Elif Nur Kalkan <elifnkalkan@gmail.com>
2022-11-29 22:37:00 +0100

    react week3 hw done

--- week3/project/ecommerce/src/App.js
+++ week3/project/ecommerce/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import ProductList from './ProductList';
 import { Routes, Route } from 'react-router-dom';
 import ProductDetail from './ProductDetail';
-import { FavoritesProvider } from './Favorites';
+import { Favorites } from './Favorites';
 import FavoriteProducts from './components/FavoriteProducts';
 import Navbar from './components/Navbar';
 
@@ -10,13 +10,13 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <FavoritesProvider>
+      <Favorites>
         <Routes>
           <Route path="/" element={<ProductList />} />
           <Route path="/favorites" element={<FavoriteProducts />} />
           <Route path="/products/:productId" element={<ProductDetail />} />
         </Routes>
-      </FavoritesProvider>
+      </Favorites>
     </div>
   );
 }
